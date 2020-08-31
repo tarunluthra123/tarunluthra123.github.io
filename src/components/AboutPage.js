3160b759f0f4ad2b2fecaf16a68e70a099021c2c
Tarun Luthra <tarunluthra987@gmail.com>
2020-08-31 14:56:42 +0530

    Skills chart

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,13 +1,51 @@
 import React, {Component} from 'react';
+import {HorizontalBar} from "react-chartjs-2";
 import me from '../assets/me.jpg';
+import skillsData from '../assets/data/skills.json'
+
+const labels = [], skillValue = []
+for (let label in skillsData) {
+    labels.push(label)
+    skillValue.push(skillsData[label])
+}
+
+const graphColorRGBCode = '34, 195, 243'
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'My Skill',
+            backgroundColor: "rgba(" + graphColorRGBCode + ",0.2)",
+            borderColor: 'rgba(' + graphColorRGBCode + ',1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(' + graphColorRGBCode + ',0.4)',
+            hoverBorderColor: 'rgba(' + graphColorRGBCode + ',1)',
+            data: skillValue,
+        }
+    ],
+    fill: false,
+}
+
+const options = {
+    scales: {
+        xAxes: [{
+            ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 100
+            },
+        }]
+    }
+}
 
 class AboutPage extends Component {
     render() {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col container-fluid">
-                        <img src={me} height="300px"/>
+                    <div className="col-4 container-fluid">
+                        <img src={me} height="250px"/>
                     </div>
                     <div className="col">
                         <h1>Hi. I am Tarun Luthra</h1>
@@ -18,6 +56,9 @@ class AboutPage extends Component {
                             I love to create web apps with React and Ember.
                         </p>
                     </div>
+                </div>
+                <div className="container">
+                    <HorizontalBar data={data} options={options} width={100} height={50}/>
                 </div>
             </div>
         );
