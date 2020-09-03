b39561b5203a8df2e50a7ddcf97aa515cf08bad4
Tarun Luthra <tarunluthra987@gmail.com>
2020-09-03 19:38:53 +0530

    AboutPage UI changes

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {HorizontalBar} from "react-chartjs-2";
-import baymax from '../assets/baymax.png';
+import me from '../assets/me.jpg';
 import skillsData from '../assets/data/skills.json'
 
 const labels = [], skillValue = []
@@ -64,27 +64,30 @@ const options = {
 class AboutPage extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-2"/>
-                    <div className="col-md-4 col-sm-6 col-6 container-fluid ">
-                        <img src={baymax} height={200} className="introductionPhoto"/>
-                    </div>
-                    <div className="col-md col-sm col">
-                        <h1>Hi. I am Tarun Luthra</h1>
-                        <p>
-                            I am a Computer Science undergrad.
-                            I am proficient competitive programmer with advanced Algo-DS skills and a trained web
-                            developer.
-                            I love to create web apps with React and Ember.
-                        </p>
+            <React.Fragment>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-2"/>
+                        <div className="col-md-4 col-sm-5 col-5 container-fluid ">
+                            <img src={me} height={window.innerWidth > 500 ? 200 : 150} className="introductionPhoto"/>
+                        </div>
+                        <div className="col-md col-sm col">
+                            <h1>Hi. I am Tarun Luthra</h1>
+                            <p>
+                                I am a Computer Science undergrad.
+                                I am proficient competitive programmer with advanced Algo-DS skills and a trained web
+                                developer.
+                                I love to create web apps with React and Ember.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="container">
                     <HorizontalBar data={data} options={options} width={100} height={50}/>
                 </div>
-            </div>
-        );
+            </React.Fragment>
+        )
+            ;
     }
 }
 
