72a00db7b9559e85cbb09033c4381ab4a2718fd7
Tarun Luthra <tarunluthra987@gmail.com>
2020-11-06 16:12:45 +0530

    Particle floating animations

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import me from '../assets/me.jpg'
 import SkillsShowcase from "./SkillsShowcase";
+import RandomParticleAnimation from "./RandomParticleAnimation";
 
 
-class AboutPage extends Component {
-    decideImageHeight = () => {
+const AboutPage = (props) => {
+    const decideImageHeight = () => {
         if (window.innerWidth > 550)
             return 240
         else if (window.innerWidth > 400)
@@ -13,35 +14,34 @@ class AboutPage extends Component {
             return 125
     }
 
-    render() {
-        return (
-            <React.Fragment>
-                <div className="aboutContainer">
-                    {window.innerWidth <= 420 &&
-                    <div align="center" className="nameHeading">Hi I am <strong>Tarun Luthra</strong></div>}
-                    <div className="row">
-                        <div className="col-lg-3"/>
-                        <div className="col-md-3 col-sm-5 col-5">
-                            <img src={me} height={this.decideImageHeight()}/>
-                        </div>
-                        <div className="col-md col-sm col">
-                            <br/>
-                            {window.innerWidth > 420 &&
-                            <div className="nameHeading">Hi I am <strong>Tarun Luthra</strong></div>}
-                            <p className="introductionText">
-                                I am a Computer Science undergrad.
-                                I am a proficient competitive programmer with advanced Algo-DS skills and a trained web
-                                developer.
-                                I love to create web apps with React and Ember.
-                            </p>
-                        </div>
+    return (
+        <React.Fragment>
+            <RandomParticleAnimation/>
+            <div className="aboutContainer">
+                {window.innerWidth <= 420 &&
+                <div align="center" className="nameHeading">Hi I am <strong>Tarun Luthra</strong></div>}
+                <div className="row">
+                    <div className="col-lg-3"/>
+                    <div className="col-md-3 col-sm-5 col-5">
+                        <img src={me} height={decideImageHeight()}/>
+                    </div>
+                    <div className="col-md col-sm col">
+                        <br/>
+                        {window.innerWidth > 420 &&
+                        <div className="nameHeading">Hi I am <strong>Tarun Luthra</strong></div>}
+                        <p className="introductionText">
+                            I am a Computer Science undergrad.
+                            I am a proficient competitive programmer with advanced Algo-DS skills and a trained web
+                            developer.
+                            I love to create web apps with React and Ember.
+                        </p>
                     </div>
                 </div>
-                <div className="diagonallyTiltedDivAbout">abcdc</div>
-                <SkillsShowcase/>
-            </React.Fragment>
-        );
-    }
+            </div>
+            <div className="diagonallyTiltedDivAbout">abcdc</div>
+            <SkillsShowcase/>
+        </React.Fragment>
+    );
 }
 
 export default AboutPage;
---
+++ src/components/RandomParticleAnimation.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import '../assets/css/RandomParticleAnimation.scss'
+
+const RandomParticleAnimation = (props) => {
+    return (
+        <div className="randomParticleAnimationWrapper">
+            <div className="particle elem1 anim-delay1"/>
+            <div className="particle elem2 anim-delay5"/>
+            <div className="particle elem3 anim-delay3"/>
+            <div className="particle elem4 anim-delay8"/>
+            <div className="particle elem5 anim-delay9"/>
+            <div className="particle elem6 anim-delay2"/>
+            <div className="particle elem7 anim-delay4"/>
+            <div className="particle elem8 anim-delay3"/>
+            <div className="particle elem9 anim-delay10"/>
+            <div className="particle elem8 anim-delay11"/>
+            <div className="particle elem6 anim-delay12"/>
+            <div className="particle elem1 anim-delay14"/>
+            <div className="particle elem13 anim-delay16"/>
+            <div className="particle elem17 anim-delay18"/>
+            <div className="particle elem2 anim-delay6"/>
+            <div className="particle elem16 anim-delay9"/>
+            <div className="particle elem8 anim-delay3"/>
+            <div className="particle elem9 anim-delay10"/>
+            <div className="particle elem18 anim-delay11"/>
+            <div className="particle elem11 anim-delay12"/>
+            <div className="particle elem12 anim-delay14"/>
+            <div className="particle elem13 anim-delay16"/>
+            <div className="particle elem14 anim-delay18"/>
+            <div className="particle elem15 anim-delay6"/>
+            <div className="particle elem16 anim-delay9"/>
+            <div className="particle elem17 anim-delay7"/>
+            <div className="particle elem18 anim-delay6"/>
+            <div className="particle elem19 anim-delay3"/>
+        </div>
+
+    )
+}
+
+export default RandomParticleAnimation;
